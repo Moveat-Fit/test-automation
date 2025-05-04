09726e4163b32b6ead985572271f1e49b4525730
tsucarol <carollyne.guim@gmail.com>
2025-05-04 18:03:54 -0300

    update cadastroNutricionista

--- cypress/e2e/Fluxo nutricionista/0-cadastroNutricionista.cy.js
+++ cypress/e2e/Fluxo nutricionista/0-cadastroNutricionista.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-import CadastroPage from '../../support/pages/CadastroPage';
+import CadastroPage from '../../support/pages/Autenticacao/CadastroPage';
 import { gerarDadosNutricionista } from '../../support/geradorDadosFaker';
 
 
