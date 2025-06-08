6ebc0bd71af3f8307e7d12327db83f082b27a7bf
tsucarol <carollyne.guim@gmail.com>
2025-06-08 12:52:02 -0300

    update criarPlanoAlimentar.cy.js

--- cypress/e2e/Fluxo nutricionista/6-criarPlanoAlimentar.cy.js
+++ cypress/e2e/Fluxo nutricionista/6-criarPlanoAlimentar.cy.js
@@ -24,5 +24,39 @@ describe('Dashboard do Nutricionista - Atualizar dados cadastrais dos pacientes'
     });
 
     /* TESTES NEGATIVOS */
+    it('Validar tratamento de erro para plano com campos obrigatórios em branco', () => {
+        CriarPlano.selecionarBtnCriarPlano();
+        cy.contains('h1', 'Criar plano alimentar').should('be.visible');
+        CriarPlano.selecionaBtnAddRefeicao();
+        CriarPlano.preencheRefeicao();
+        CriarPlano.selecionaBtnAddAlimento();
+        // Não seleciona alimento
+        CriarPlano.selecionaBtnSalvarPlano();
+        
+        cy.contains('Nome do plano é obrigatório').should('be.visible');
+        cy.contains('Data de início é obrigatória').should('be.visible');
+        //cy.contains('Data de término é obrigatória').should('be.visible');
+        cy.contains('Selecionar um alimento é obrigatório').should('be.visible');
+        cy.contains('Porção é obrigatória').should('be.visible');
+    });
 
+    it('Validar tratamento de erro para plano alimentar sem refeições', () => {
+        CriarPlano.selecionarBtnCriarPlano();
+        cy.contains('h1', 'Criar plano alimentar').should('be.visible');
+        CriarPlano.preencheDetalhesPlano();
+        // Não adiciona refeições
+        CriarPlano.selecionaBtnSalvarPlano();
+        cy.contains('Inclua pelo menos 1 refeição').should('be.visible');
+    });
+
+    it('Validar tratamento de erro para refeição sem alimentos', () => {
+        CriarPlano.selecionarBtnCriarPlano();
+        cy.contains('h1', 'Criar plano alimentar').should('be.visible');
+        CriarPlano.preencheDetalhesPlano();
+        CriarPlano.selecionaBtnAddRefeicao();
+        CriarPlano.preencheRefeicao();
+        // Não adiciona alimentos
+        CriarPlano.selecionaBtnSalvarPlano();
+        cy.contains('Nenhum alimento adicionado ainda. Clique em "Adicionar alimento" para começar').should('be.visible');
+    });
 })
